0462ad9f84829d856c57d87d5650f59ea4071937
andriib <a,bendes10@gmail.com>
2022-04-06 22:38:22 +0300

    Add project files.

---
+++ ConsoleApp4/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ConsoleApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.createBtn = new System.Windows.Forms.Button();
+            this.picMaze = new System.Windows.Forms.PictureBox();
+            this.solveBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picMaze)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 17);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "width";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(202, 17);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "height";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(71, 14);
+            this.txtWidth.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(96, 22);
+            this.txtWidth.TabIndex = 2;
+            this.txtWidth.Text = "10";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(257, 14);
+            this.txtHeight.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(88, 22);
+            this.txtHeight.TabIndex = 2;
+            this.txtHeight.Text = "10";
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(363, 11);
+            this.createBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(100, 28);
+            this.createBtn.TabIndex = 3;
+            this.createBtn.Text = "create";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // picMaze
+            // 
+            this.picMaze.Location = new System.Drawing.Point(16, 89);
+            this.picMaze.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.picMaze.MinimumSize = new System.Drawing.Size(420, 260);
+            this.picMaze.Name = "picMaze";
+            this.picMaze.Size = new System.Drawing.Size(555, 304);
+            this.picMaze.TabIndex = 4;
+            this.picMaze.TabStop = false;
+            // 
+            // solveBtn
+            // 
+            this.solveBtn.Location = new System.Drawing.Point(471, 11);
+            this.solveBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.solveBtn.Name = "solveBtn";
+            this.solveBtn.Size = new System.Drawing.Size(100, 28);
+            this.solveBtn.TabIndex = 6;
+            this.solveBtn.Text = "Solve";
+            this.solveBtn.UseVisualStyleBackColor = true;
+            this.solveBtn.Click += new System.EventHandler(this.solveBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(584, 406);
+            this.Controls.Add(this.solveBtn);
+            this.Controls.Add(this.picMaze);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "MazeGenerator";
+            ((System.ComponentModel.ISupportInitialize)(this.picMaze)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Button createBtn;
+        private System.Windows.Forms.PictureBox picMaze;
+        private System.Windows.Forms.Button solveBtn;
+    }
+}
